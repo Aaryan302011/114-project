f211bf2e8b49a5a38410c1b1bef34b8ae742fce1
Aaryan302011 <110681706+Aaryan302011@users.noreply.github.com>
2023-08-11 15:14:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(300,250);
+    canvas.center();
+}
+
+function draw(){
+
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
